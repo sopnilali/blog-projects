6d6bec10701a36b96f5e1a954e21ab49e9457ba3
Md Abdul Adud <sopnilali24@gmail.com>
2024-12-21 00:13:51 +0600

    added

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, ErrorRequestHandler, NextFunction, Request, Response } from 'express'
 import router from './routes'
 const app : Application = express()
 
@@ -9,10 +9,39 @@ app.use('/api', router)
 
 
 app.get('/', (req: Request, res: Response) => {
-    res.json({
-        status: true,
-        message: 'Welcome to Blog Project API',
+    res.send({
+      status: true,
+      message: 'Welcome to the Blog Project API! ⚡',
     })
-})
+  })
+
+// error handling middleware start
+
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+    const error = new Error(`Could not found ${req.url}`)
+    res.status(404)
+    res.json({
+      status: false,
+      message: error.message,
+      stack: error.stack,
+    })
+    next(error)
+  })
+  
+  app.use(
+    (
+      err: ErrorRequestHandler,
+      req: Request,
+      res: Response,
+      next: NextFunction
+    ) => {
+      res.status(500).json({
+        status: false,
+        message: 'Internal Server Error',
+      })
+      next(err)
+    }
+  )
+  // error handling middleware end
 
 export default app
